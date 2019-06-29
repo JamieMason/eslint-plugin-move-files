3446d84f218ed902865aa9cb95bceb80494ccbc7
Jamie Mason <jamie@foldleft.io>
2019-06-29 20:45:30 +0100

    feat(rule): update external imports of a renamed file

--- src/move-files.spec.ts
+++ src/move-files.spec.ts
@@ -1,7 +1,11 @@
-import { existsSync } from 'fs';
+import { readFileSync } from 'fs';
 import * as mock from 'mock-fs';
 import rule from './move-files';
 import { ruleTester } from './test/rule-tester';
+
+beforeAll(() => {
+  console.log('https://github.com/facebook/jest/issues/5792');
+});
 
 describe('when no files are provided', () => {
   [[{ files: null }]].forEach((options: any) => {
@@ -17,47 +21,106 @@ describe('when no files are provided', () => {
 });
 
 describe('when renaming one file', () => {
-  const rootDir = '/fake/dir';
-  const oldName = 'old-name.js';
-  const newName = 'new-name.js';
-  const oldPath = `${rootDir}/${oldName}`;
-  const newPath = `${rootDir}/${newName}`;
-  const fileContents = 'export const a = 1;';
+  const oldPath = `/fake/dir/old-name.js`;
+  const newPath = `/fake/dir/new-name.js`;
+  const fileContents = `
+    import { dep } from './dep';
+    export const a = 1;
+  `;
+  const errors = [{ message: `${oldPath} has moved to ${newPath}` }];
+  const options = [{ files: { [oldPath]: newPath } }];
 
   describe('when file already has the new name', () => {
     it('is valid', () => {
       ruleTester.run('move-files', rule, {
-        valid: [
-          {
-            code: '',
-            filename: newPath,
-            options: [{ files: { [oldPath]: newPath }, rootDir }]
-          }
-        ],
+        valid: [{ code: '', filename: newPath, options }],
         invalid: []
       });
     });
   });
 
   describe('when file has the old name', () => {
-    it('renames the file', () => {
-      const message = `${oldPath} has moved to ${newPath}`;
-      mock({ [rootDir]: { [oldName]: fileContents } });
+    beforeEach(() => {
+      mock({ [oldPath]: fileContents });
+    });
 
+    afterEach(() => {
+      mock.restore();
+    });
+
+    it('renames the file', () => {
       ruleTester.run('move-files', rule, {
         valid: [],
         invalid: [
           {
             code: fileContents,
-            errors: [{ message }],
+            errors,
             filename: oldPath,
-            options: [{ files: { [oldPath]: newPath }, rootDir }]
+            options
           }
         ]
       });
+      const newPathContents = readFileSync(newPath, { encoding: 'utf8' });
+      expect(newPathContents).toEqual(fileContents);
+    });
 
-      expect(existsSync(newPath)).toEqual(true);
-      mock.restore();
+    it('updates imports to the renamed file', () => {
+      ruleTester.run('move-files', rule, {
+        valid: [
+          {
+            code: `import { a } from './new-name';`,
+            filename: `/fake/dir/sibling.js`
+          },
+          {
+            code: `import { a } from '../new-name';`,
+            filename: `/fake/dir/dir/child.js`
+          },
+          {
+            code: `import { a } from './fake/dir/new-name';`,
+            filename: `/parent.js`
+          }
+        ],
+        invalid: [
+          {
+            code: `import { a } from './old-name';`,
+            filename: `/fake/dir/sibling.js`,
+            output: `import { a } from './new-name';`
+          },
+          {
+            code: `import { a } from '../old-name';`,
+            filename: `/fake/dir/dir/child.js`,
+            output: `import { a } from '../new-name';`
+          },
+          {
+            code: `import { a } from './fake/dir/old-name';`,
+            filename: `/parent.js`,
+            output: `import { a } from './fake/dir/new-name';`
+          }
+        ].map((spec) => ({ ...spec, errors, options }))
+      });
+    });
+
+    it('updates requires to the renamed file', () => {
+      ruleTester.run('move-files', rule, {
+        valid: [],
+        invalid: [
+          {
+            code: `const { a } = require('./old-name');`,
+            filename: `/fake/dir/sibling.js`,
+            output: `const { a } = require('./new-name');`
+          },
+          {
+            code: `const { a } = require('../old-name');`,
+            filename: `/fake/dir/dir/child.js`,
+            output: `const { a } = require('../new-name');`
+          },
+          {
+            code: `const { a } = require('./fake/dir/old-name');`,
+            filename: `/parent.js`,
+            output: `const { a } = require('./fake/dir/new-name');`
+          }
+        ].map((spec) => ({ ...spec, errors, options }))
+      });
     });
   });
 });
--- src/move-files.ts
+++ src/move-files.ts
@@ -1,6 +1,7 @@
 import { Rule } from 'eslint';
-import { Node } from 'estree';
+import * as EsTree from 'estree';
 import { moveSync } from 'fs-extra';
+import { dirname, relative, resolve } from 'path';
 import { ERROR_MOVED_FILE } from './config';
 import { getIn } from './lib/get-in';
 
@@ -22,33 +23,83 @@ const rule: Rule.RuleModule = {
             type: 'object',
             patternProperties: { '^.*$': { anyOf: [{ type: 'string' }] } },
             additionalProperties: false
-          },
-          rootDir: { type: 'string' }
+          }
         }
       }
     ]
   },
   create: (context) => {
     const files = getIn('options.0.files', context, {});
-    const rootDir = getIn('options.0.rootDir', context, process.cwd());
-    const oldPath = context.getFilename();
-    const newPath = files[oldPath];
+    const currentFilePath = context.getFilename();
+    const dirPath = dirname(currentFilePath);
 
-    if (newPath) {
-      return {
-        'Program:exit': (node: Node) =>
+    const withLeadingDot = (moduleId: string) =>
+      moduleId.startsWith('.') ? moduleId : `./${moduleId}`;
+
+    const withoutFileExtension = (filePath: string) =>
+      filePath.replace(/\.[^.]+$/, '');
+
+    const getNewModuleId = (newPath: string) =>
+      withLeadingDot(withoutFileExtension(relative(dirPath, newPath)));
+
+    const withFileExtension = (filePath: string) => {
+      try {
+        return require.resolve(filePath);
+      } catch (e) {
+        return filePath;
+      }
+    };
+
+    return {
+      'CallExpression[callee.name="require"][arguments.0.value=/^\\./]'(
+        n: EsTree.Node
+      ) {
+        const node = n as any;
+        const moduleId = node.arguments[0].value;
+        const quotes = node.arguments[0].raw.charAt(0);
+        const modulePath = withFileExtension(resolve(dirPath, moduleId));
+        const newModulePath = files[modulePath];
+        if (newModulePath) {
+          const newModuleId = getNewModuleId(newModulePath);
+          const withQuotes = `${quotes}${newModuleId}${quotes}`;
+          context.report({
+            fix: (fixer) => fixer.replaceText(node.arguments[0], withQuotes),
+            message: ERROR_MOVED_FILE(modulePath, newModulePath),
+            node
+          });
+        }
+      },
+      'ImportDeclaration[source.value=/^\\./]'(n: EsTree.Node) {
+        const node = n as any;
+        const moduleId = node.source.value;
+        const quotes = node.source.raw.charAt(0);
+        const modulePath = withFileExtension(resolve(dirPath, moduleId));
+        const newModulePath = files[modulePath];
+        if (newModulePath) {
+          const newModuleId = getNewModuleId(newModulePath);
+          const withQuotes = `${quotes}${newModuleId}${quotes}`;
+          context.report({
+            fix: (fixer) => fixer.replaceText(node.source, withQuotes),
+            message: ERROR_MOVED_FILE(modulePath, newModulePath),
+            node
+          });
+        }
+      },
+      'Program:exit'(n: EsTree.Node) {
+        const node = n as EsTree.Program;
+        const newFilePath = files[currentFilePath];
+        if (newFilePath) {
           context.report({
             fix(fixer) {
-              moveSync(oldPath, newPath, { overwrite: true });
+              moveSync(currentFilePath, newFilePath, { overwrite: true });
               return fixer.insertTextAfter(node, '');
             },
-            message: ERROR_MOVED_FILE(oldPath, newPath),
+            message: ERROR_MOVED_FILE(currentFilePath, newFilePath),
             node
-          })
-      };
-    }
-
-    return {};
+          });
+        }
+      }
+    };
   }
 };
 
